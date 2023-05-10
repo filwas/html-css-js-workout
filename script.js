4c5e3959175a6d4e8790afa9a7d83c33378e389d
Filip Was <fwas.corp@gmail.com>
2023-05-10 18:12:58 +0200

    change color function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello from Nerdbord!')
+
+function changeBorderColour() {
+    let colours = ["red", "green", "blue", "cyan", "magenta", "yellow", "black"];
+    let random = Math.floor(Math.random()*colours.length)
+    document.getElementsByClassName("box")[0].style.borderColor = colours[random];
+}
+
+setInterval(changeBorderColour, 3000)
